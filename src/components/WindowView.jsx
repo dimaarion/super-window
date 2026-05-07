3bb8f84bdf7a76fd05bda5ccede123035ec257dc
dimaarion <dimaarion@gmail.com>
2026-05-07 06:38:34 +0300

    /dimaarion/super-window

--- src/components/WindowView.jsx
+++ src/components/WindowView.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import {useEffect, useState} from "react";
 
 export default function WindowConstructor({
                                               width = 1500, // Внешний параметр ширины
